8b2e1930d5311380e77c6d4fd92ebcb8ad0d16ee
kamranss <kamranss@code.edu.az>
2023-02-27 23:19:24 +0400

    Refactor: task updated

--- assets/scripts/highlightBox.js
+++ assets/scripts/highlightBox.js
@@ -2,10 +2,11 @@ let button = document.querySelectorAll(".btn");
 let clickedBox;
 button.forEach(btn=>{
     btn.addEventListener("click", function(){
-        clickedBox? clickedBox.style.backroundColor = "transparent":"";
+        clickedBox?clickedBox.style.backroundColor = "transparent":"";
         let dataId = this.getAttribute("data-id");
         document.getElementById(dataId).style.backgroundColor = dataId;
         clickedBox = document.getElementById(dataId);
-        
+        console.log(clickedBox);
+    
     })
 })
